0dcb6cc65b7fc07d79b30fa2b67aad2b7d661770
Jlrajkumar <jlrkumarjaddu@gmail.com>
2021-01-31 00:29:44 +0000

    fixed index.js errors

--- index.js
+++ index.js
@@ -5,15 +5,7 @@ const app = express();
 // or somehow registered with "app" obj here
 
 app.get('/', (req,res) => {
-    res.send('Hiya 👋 , I'm J L Raj Kumar
-    🔭 I’m currently excelling on Javascript
-    🌱 I’m currently learning ReactJS
-    ⚡ I'm Enthusiastic to learn Cloud technologies
-    👯 I’m looking to collaborate on Open Source projects
-    💬 Ask me about SAP-ABAP|S4-HANA|Javascript|HTML|CSS|Flex-Box|UML....
-    ⚡ Pro-tip: Knowledge is Divine! Inorder to GAIN more UNFURL more!!
-    Connect... 🤝🏻
-    @Linkedin - JLRajkumar!');
+    res.send('Hello I am J L Rajkumar');
 });
 
 const PORT = process.env.PORT || 5000;
