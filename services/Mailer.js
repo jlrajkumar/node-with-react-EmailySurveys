a3539d435bc2edaf5574e24240ad54802f33bcb9
Jlrajkumar <jlrkumarjaddu@gmail.com>
2021-07-06 18:31:33 +0100

    Added Sending Email Functionality using SendGrid API

--- services/Mailer.js
+++ services/Mailer.js
@@ -5,6 +5,8 @@ class Mailer extends helper.Mail{
     constructor({subject, recipients}, content){
         super();
 
+        this.sgAPI = sendgrid(keys.sendGridKey);
+
         this.from_email = new helper.Email('jlrkumarjaddu@gmail.com');
         this.subject = subject;
         this.body = new helper.Content('text/html', content);
@@ -33,6 +35,29 @@ class Mailer extends helper.Mail{
         this.addTrackingSettings(trackingSettings);
 
     }
+
+
+    addRecipients(){
+        const personalize = new helper.Personalization();
+        this.recipients.forEach(recipient => {
+            personalize.addTo(recipient);
+        });
+         this.addPersonalization(personalize);
+    }
+
+   async send() {
+            const request = this.sgAPI.emptyRequest({
+                    method:'POST', 
+                    path: 'v3/mail/send',
+                    body:this.toJSON()
+            });
+
+        const response = this.sgAPI.API(request);
+        return response;
+    }
+
+
+
 }
 
 module.exports = Mailer;
