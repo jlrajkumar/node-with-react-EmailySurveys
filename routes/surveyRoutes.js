ad9fbbae46d560e9269c0a1159e6fd6353b6c842
Jlrajkumar <jlrkumarjaddu@gmail.com>
2021-07-06 20:18:34 +0100

    Send Email to user and when responds to f/b redirect to Thanks

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -3,12 +3,18 @@ const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 const Mailer = require('../services/Mailer');
 const surveyTemplate = require('../services/email Templates/surveyTemplate');
+const { request } = require('express');
+
 
 const Survey = mongoose.model('surveys');
 
 module.exports = app =>{
 
-    app.post('/api/surveys', requireLogin, requireCredits, (req,res) => {
+    app.get('/api/surveys/thanks', (req,res)=>{
+        res.send('Thanks for submitting your feedback!.');
+    });
+
+    app.post('/api/surveys', requireLogin, requireCredits, async (req,res) => {
 
         const { title, subject, body, recipients } = req.body;
 
@@ -24,7 +30,20 @@ module.exports = app =>{
             });
 
             //Send an email 
-            const mailer = new Mailer(survey, template);
+        
+    const mailer = new Mailer(survey, surveyTemplate(survey));
+        try{
+                    await mailer.send();
+
+                    await survey.save();
+                    req.user.credits -= 1;
+                const user =   await  req.user.save();
+                res.send(user);
+    } catch(err){
+        res.status(422).send(err);
+      }
+
+    
 
     });
  };
